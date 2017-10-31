550bf74113b67f0df4015861f1d918432b75009f
baristikk <filipea133@gmail.com>
2017-10-31 12:12:03 -0200

    Controlando as animacoes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,56 +2,88 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+Idle - 0
+Jump - 1
+Run - 2
+Falling - 3
+Shooting - 4
+Hurt - 5
+*/
+
 public class PlayerController : MonoBehaviour {
 
-
-	public float horizontalSpeed = 10f;
-
-	public float jumpSpeed = 600f; 
+	public float horizontalSpeed = 5f;
+	public float jumpSpeed = 600f;
 
 	Rigidbody2D rb;
-	SpriteRenderer sr; 
+	SpriteRenderer sr;
+	Animator anim;
 
-	// Use this for initialization
+	bool isJumping = false;
+
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		sr = GetComponent<SpriteRenderer>();
+		anim = GetComponent<Animator>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-
-		float horizontalInput = Input.GetAxisRaw("Horizontal");
+		float horizontalInput = Input.GetAxisRaw("Horizontal"); // -1 = esquerda, 1 = direita,
 		float horizontalPlayerSpeed = horizontalSpeed * horizontalInput;
-
-		if(horizontalSpeed != 0) {
+		if (horizontalPlayerSpeed != 0){
 			MoveHorizontal(horizontalPlayerSpeed);
 		}
 		else{
 			StopMovingHorizontal();
 		}
 
-		if(Input.GetButtonDown("Jump")){
+		if (Input.GetButtonDown("Jump")){
 			Jump();
-		 }
-	}
+		}
 
+		ShowFalling();
+	}
 
 	void MoveHorizontal(float speed){
 		rb.velocity = new Vector2(speed, rb.velocity.y);
 
-		if(speed < 0f) {
+		if (speed < 0f){
 			sr.flipX = true;
 		}
-		else if (speed > 0f) {
+		else if (speed > 0f) {	
 			sr.flipX = false;
+		}
+
+		if(!isJumping){
+		anim.SetInteger("State", 2);
 		}
 	}
 
 	void StopMovingHorizontal(){
 		rb.velocity = new Vector2(0f, rb.velocity.y);
+		if(!isJumping){
+		anim.SetInteger("State", 0);
+		}
+
 	}
+
+	void ShowFalling(){
+		if(rb.velocity.y < 0f){
+			anim.SetInteger("State",3);
+		}
+	}
+
 	void Jump(){
+		isJumping = true;
 		rb.AddForce(new Vector2(0f, jumpSpeed));
+
+		anim.SetInteger("State", 1);
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if(other.gameObject.layer == LayerMask.NameToLayer("Ground")){
+			isJumping = false;
+		}
 	}
 }

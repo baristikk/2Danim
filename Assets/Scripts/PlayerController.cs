0ed3ffcc45df9c1e2c9177c1d765d7cdf906df91
baristikk <filipea133@gmail.com>
2017-11-21 11:32:44 -0200

    Adicionando sons

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -101,6 +101,7 @@ public class PlayerController : MonoBehaviour {
 
 		if (isGrounded){
 			isJumping = true;
+			AudioManager.instance.PlayJumpSound(gameObject);
 			rb.AddForce(new Vector2(0f, jumpSpeed));
 
 			anim.SetInteger("State", 1);
@@ -111,6 +112,7 @@ public class PlayerController : MonoBehaviour {
 		
 		if (canDoubleJump && !isGrounded){
  			rb.velocity = Vector2.zero;
+			AudioManager.instance.PlayJumpSound(gameObject);
  			rb.AddForce(new Vector2(0f, jumpSpeed));
  			anim.SetInteger("State", 1);
  			canDoubleJump = false;
@@ -128,6 +130,7 @@ public class PlayerController : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other){
 		if(other.gameObject.CompareTag("Coin")){
+			AudioManager.instance.PlayCoinPickupSoud(other.gameObject);
 			SFXManager.instance.ShowCoinParticles(other.gameObject);
 			Destroy(other.gameObject);
 		}

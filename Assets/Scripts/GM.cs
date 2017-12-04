2d4af24b1174b8bf7cb9d503b54720b90f036b14
baristikk <filipea133@gmail.com>
2017-12-04 18:15:00 -0200

    Machucando o player

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -14,6 +14,7 @@
  	PlayerController player;
  
  	public float timeToRespawn = 1.5f;
+	public float timeToKill = 1.5f;
  
 
 	public float maxTime = 120f;
@@ -131,5 +132,32 @@
 		ui.levelComplete.LevelCompletePanel.SetActive(true);
 		
 	}
-	
+
+	public void HurtPlayer(){
+		if (player != null){
+			DisableAndPushPlayer();
+			Destroy(player.gameObject, timeToKill);
+			DecrementLives();
+			if (data.lifeCount > 0){
+			Invoke("RespawnPlayer", timeToKill + timeToRespawn);
+		}
+		else {
+			GameOver();
+		}
+	}
+	}
+
+    void DisableAndPushPlayer(){
+		player.transform.GetComponent<PlayerController>().enabled = false;
+		foreach (Collider2D c2d in player.transform.GetComponents<Collider2D>()){
+			c2d.enabled = false;
+		}
+		foreach (Transform child in player.transform){
+			child.gameObject.SetActive(false);
+		}
+		Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+		rb.velocity = Vector2.zero;
+		rb.AddForce(new Vector2(-150.0f, 400f));
+	}
+
  } 
